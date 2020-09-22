a0cc2a5d58571639a4848fdf263a27ce19d8d632
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-22 12:37:20 -0600

    Route setup routes to about & home working

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title }) => {
   return (
@@ -6,6 +7,14 @@ const Navbar = ({ title }) => {
       <h1>
         <i className="fab fa-github" /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
