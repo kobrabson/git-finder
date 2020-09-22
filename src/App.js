65221f105aba63360fc7525ab92f1907d1ec5e15
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-22 11:05:52 -0600

    search bar can be use in other projects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import axios from "axios";
 import "./App.css";
 
@@ -12,17 +13,26 @@ class App extends Component {
 
   async componentDidMount() {
     this.setState({ loading: true });
-
     const res = await axios.get("https://api.github.com/users");
-
     this.setState({ users: res.data, loading: false });
   }
+
+  // Search Github Users passed by props from Search.js
+
+  searchUsers = async (text) => {
+    // console.log(text);
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
